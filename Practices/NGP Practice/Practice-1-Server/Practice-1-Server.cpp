a618dd8793d8537c21326b35dba04100e432b7e2
Iconer <yoyofa2@hotmail.com>
2022-02-09 13:58:16 +0900

    init

---
+++ Practices/NGP Practice/Practice-1-Server/Practice-1-Server.cpp
@@ -0,0 +1,6 @@
+﻿#include "pch.h"
+
+
+int main() {
+	std::cout << "Hello World!\n";
+}
---
+++ Practices/NGP Practice/Practice-1-Server/pch.h
@@ -0,0 +1,24 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <WinSock2.h>
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <chrono>
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::vector;
+using std::map;
+using std::begin;
+using std::end;
+using std::cbegin;
+using std::cend;
+using std::chrono::steady_clock;
+using std::chrono::system_clock;
+
+#endif //PCH_H
