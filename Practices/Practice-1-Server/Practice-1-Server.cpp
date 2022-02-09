2da2197359af87548e056757a746f53b9de30472
Iconer <yoyofa2@hotmail.com>
2022-02-09 18:22:55 +0900

    thread & future test

--- Practices/Practice-1-Server/Practice-1-Server.cpp
+++ Practices/Practice-1-Server/Practice-1-Server.cpp
@@ -1,6 +1,50 @@
 ﻿#include "pch.h"
 
+constexpr int PORT = 40000;
+
+
+void _Server_start(promise<bool>&);
+void _Server_update();
 
 int main() {
-	std::cout << "Hello World!\n";
+	std::cout << "Server initiated.\n";
+
+	auto initiator = promise<bool>();
+	auto awaiter = initiator.get_future();
+
+	auto init_worker = thread(_Server_start, initiator);
+	
+	auto period = std::chrono::seconds(3);
+	auto result = awaiter.wait_for(period);
+
+	if (std::future_status::ready == result) {
+		try {
+			auto status = awaiter.get();
+
+			if (status) {
+
+			} else {
+
+			}
+		} catch (exception& e) {
+			cout << "Error: " << e.what() << std::endl;
+		}
+	}
+	
+	init_worker.join();
+}
+
+void _Server_start(promise<bool>& result) {
+	WSADATA wsadata;
+
+	try {
+		auto status = WSAStartup(MAKEWORD(2, 2), &wsadata);
+		if (0 != status) {
+			result.set_exception(make_exception_ptr("Startup error!"));
+		} else {
+			result.set_value(true);
+		}
+	} catch (...) {
+		result.set_exception(std::current_exception());
+	}
 }
--- Practices/Practice-1-Server/pch.h
+++ Practices/Practice-1-Server/pch.h
@@ -2,22 +2,40 @@
 #define PCH_H
 
 #include <WinSock2.h>
+#include <Windows.h>
 
+#include <string>
+#include <span>
 #include <iostream>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <exception>
+#include <memory>
 
 using std::cout;
 using std::cin;
 using std::cerr;
 using std::vector;
 using std::map;
+using std::thread;
+using std::mutex;
+using std::future;
+using std::promise;
+using std::shared_ptr;
+using std::unique_ptr;
+using std::exception;
+using std::make_shared;
+using std::make_unique;
 using std::begin;
 using std::end;
 using std::cbegin;
 using std::cend;
+using std::make_exception_ptr;
 using std::chrono::steady_clock;
 using std::chrono::system_clock;
 
