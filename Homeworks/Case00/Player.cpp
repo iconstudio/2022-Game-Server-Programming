be412c5d8ef4b53004883b2e920ec1e232ed4863
Iconer <yoyofa2@hotmail.com>
2022-03-08 21:23:10 +0900

    Player Moves & Framerate to 30

--- Homeworks/Case00/Player.cpp
+++ Homeworks/Case00/Player.cpp
@@ -3,12 +3,38 @@
 
 void Player::Update(float delta_time)
 {
-	auto check_lt = GetAsyncKeyState(VK_LEFT);
-	auto check_up = GetAsyncKeyState(VK_UP);
-	auto check_rt = GetAsyncKeyState(VK_RIGHT);
-	auto check_dw = GetAsyncKeyState(VK_DOWN);
+	auto val_lt = GetAsyncKeyState(VK_LEFT);
+	auto val_up = GetAsyncKeyState(VK_UP);
+	auto val_rt = GetAsyncKeyState(VK_RIGHT);
+	auto val_dw = GetAsyncKeyState(VK_DOWN);
 
+	bool check_lt = 0x8000 & val_lt;
+	bool check_up = 0x8000 & val_up;
+	bool check_rt = 0x8000 & val_rt;
+	bool check_dw = 0x8000 & val_dw;
 
+	if (check_lt ^ check_rt)
+	{
+		if (check_lt)
+		{
+			MoveLT();
+		}
+		else if (check_rt)
+		{
+			MoveRT();
+		}
+	}
+	if (check_up ^ check_dw)
+	{
+		if (check_up)
+		{
+			MoveUP();
+		}
+		else if (check_dw)
+		{
+			MoveDW();
+		}
+	}
 }
 
 void Player::Render(HDC canvas)
--- Homeworks/Case00/main.cpp
+++ Homeworks/Case00/main.cpp
@@ -7,7 +7,7 @@
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
-WindowsForm window{ WND_SZ_W, WND_SZ_H, 100 };
+WindowsForm window{ WND_SZ_W, WND_SZ_H, 30 };
 Framework framework{};
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -77,7 +77,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 	}
-	return 0;
 }
 
 HGDIOBJ Draw::Attach(HDC canvas, HGDIOBJ object)
