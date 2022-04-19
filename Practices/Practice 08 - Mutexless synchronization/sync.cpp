f13a668b557e8f5f916969d36c4b7033369a90e9
Iconer <yoyofa2@hotmail.com>
2022-04-19 15:23:28 +0900

    Practicing synchronizations

---
+++ Practices/Practice 08 - Mutexless synchronization/sync.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+
+int s_data = 0;
+bool ready = false;
+
+void Receive();
+void Send();
+
+/*
+	릴리스 모드에선 버그가 발생한다.
+	버그 1: VS의 컴파일러 버그
+	버그 2: 컴파일러에서 while을 검사할 때 조건 변수 갱신을 안 한다.
+			사실 이건 버그가 아니다! C언어 자체가 멀티스레드 언어가 아니기 때문이다.
+*/
+int main()
+{
+	std::thread worker1{ Receive };
+	std::thread worker2{ Send };
+
+	worker1.join();
+	worker2.join();
+
+	return 0;
+}
+
+void Receive()
+{
+	// 반복문 내용이 비어있으면 릴리스 모드에선 날아가버린다.
+	while (!ready)
+	{
+		// ... vs 버그
+		std::this_thread::yield();
+	}
+
+	std::cout << "Receive: " << s_data << ".\n";
+}
+
+void Send()
+{
+	s_data = 999;
+	ready = true;
+}
