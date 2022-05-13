fe256b292c218aac651122c8fbd9704e246fcabb
Iconer <yoyofa2@hotmail.com>
2022-05-13 16:12:20 +0900

    필요한 클래스 존재의 선언

--- Homeworks/Case 05 - Expanded World Server/stdafx.hpp
+++ Homeworks/Case 05 - Expanded World Server/stdafx.hpp
@@ -17,8 +17,23 @@
 #include <concurrent_vector.h>
 
 class IOCPFramework;
+class Timer;
+
+class Asynchron;
+class Packet;
+
 class Session;
 enum class SESSION_STATES;
+class LocalSession;
+class SightSector;
+class SightInstances;
+
+class GameTransform;
+class GameObject;
+class GameStaticObject;
+
+class PlayerCharacter;
+class NonPlayerable;
 
 /// <summary>
 /// 플레이어 캐릭터
@@ -37,15 +52,3 @@ public:
 };
 
 void ErrorDisplay(const char* title);
-
-constexpr SIZE_T WND_SZ_W = 800, WND_SZ_H = 600; // 창 크기
-constexpr SIZE_T CELL_SIZE = 64;
-constexpr int CELL_W = CELL_SIZE;
-constexpr int CELL_H = CELL_SIZE;
-constexpr int CELLS_CNT_H = 8;
-constexpr int CELLS_CNT_V = 8;
-constexpr int CELLS_LENGTH = CELLS_CNT_H * CELLS_CNT_V;
-constexpr int BOARD_W = CELL_W * CELLS_CNT_H;
-constexpr int BOARD_H = CELL_H * CELLS_CNT_V;
-constexpr int BOARD_X = (WND_SZ_W - BOARD_W) / 2;
-constexpr int BOARD_Y = (WND_SZ_H - BOARD_H - 20) / 2;
