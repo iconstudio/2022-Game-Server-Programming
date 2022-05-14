33f49bbe659c40d3f02c96d7b9f15f2e06aeff49
Iconer <yoyofa2@hotmail.com>
2022-05-14 18:37:11 +0900

    구역을 들어오고 나가는 메서드 선언

--- Homeworks/Case 05 - Expanded World Server/GameObject.hpp
+++ Homeworks/Case 05 - Expanded World Server/GameObject.hpp
@@ -3,4 +3,13 @@
 #include "GameEntity.hpp"
 
 class GameObject : public GameEntity
-{};
+{
+public:
+	GameObject();
+	virtual ~GameObject();
+
+	void Enter(SightSector& sector);
+	void Leave(SightSector& sector);
+
+	SightSector* mySector;
+};
