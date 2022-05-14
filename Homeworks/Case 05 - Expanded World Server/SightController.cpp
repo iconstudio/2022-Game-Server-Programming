3a31a7aff9b4ef47d96b1d6ddb021657a447af62
Iconer <yoyofa2@hotmail.com>
2022-05-14 18:42:27 +0900

    시야 관리 객체 초기화

--- Homeworks/Case 05 - Expanded World Server/SightController.cpp
+++ Homeworks/Case 05 - Expanded World Server/SightController.cpp
@@ -1,7 +1,12 @@
 #include "stdafx.hpp"
 #include "SightController.hpp"
 
-SightController::SightController(float width, float height, int cell_w, int cell_h)
+SightController::SightController(float width, float height, int sector_w, int sector_h)
+	: worldHrzSize(width), worldVrtSize(height)
+	, worldSectorHrzSize(sector_w), worldSectorVrtSize(sector_h)
+	, countHrzSectors(size_t(width / sector_w) + 1)
+	, countVrtSectors(size_t(height / sector_h) + 1)
+	, mySectors(nullptr)
 {
 
 }
@@ -12,12 +17,28 @@ SightController::~SightController()
 void SightController::Update()
 {}
 
-SightSector& SightController::Get(int x, int y)
+SightSector*& SightController::Get(int x, int y) const
 {
-
+	return (mySectors.get())[y][x];
 }
 
-SightSector& SightController::GetByPosition(float x, float y)
+SightSector*& SightController::GetByPosition(float x, float y) const
 {
+	const auto coords = PickCoords(x, y);
+	return (mySectors.get())[coords.first][coords.second];
+}
 
+constexpr int_pair SightController::PickCoords(float x, float y) const
+{
+	return int_pair();
+}
+
+constexpr float_pair SightController::PickPositionFirst(int x, int y) const
+{
+	return float_pair();
+}
+
+constexpr float_pair SightController::PickPositionLast(int x, int y) const
+{
+	return float_pair();
 }
--- Homeworks/Case 05 - Expanded World Server/SightController.hpp
+++ Homeworks/Case 05 - Expanded World Server/SightController.hpp
@@ -1,21 +1,54 @@
 #pragma once
 #include "SightSector.hpp"
 
+using int_pair = std::pair<int, int>;
+using float_pair = std::pair<float, float>;
+
 class SightController
 {
 public:
-	SightController(float width, float height, int cell_w, int cell_h);
+	SightController(float width, float height, int sector_w, int sector_h);
 	~SightController();
 
 	void Update();
 
-	SightSector& Get(int x, int y);
-	SightSector& GetByPosition(float x, float y);
+	SightSector*& Get(int x, int y) const;
+	SightSector*& GetByPosition(float x, float y) const;
 
-	const size_t countHrzSectors;
-	const size_t countVrtSectors;
 	const float worldHrzSize;
 	const float worldVrtSize;
 
+	const float worldSectorHrzSize;
+	const float worldSectorVrtSize;
+
+	const size_t countHrzSectors;
+	const size_t countVrtSectors;
+
+	/// <summary>
+	/// 행우선 행렬: [Y][X]
+	/// </summary>
 	const unique_ptr<SightSector**> mySectors;
+
+private:
+	/// <summary>
+	/// 월드의 좌표에 위치한 영역의 번호
+	/// </summary>
+	/// <param name="x">월드 X 좌표</param>
+	/// <param name="y">월드 Y 좌표</param>
+	/// <returns></returns>
+	constexpr int_pair PickCoords(float x, float y) const;
+	/// <summary>
+	/// 영역의 왼쪽 위 월드 좌표
+	/// </summary>
+	/// <param name="x">영역의 가로 번호</param>
+	/// <param name="y">영역의 세로 번호</param>
+	/// <returns></returns>
+	constexpr float_pair PickPositionFirst(int x, int y) const;
+	/// <summary>
+	/// 영역의 오른쪽 아래 월드 좌표
+	/// </summary>
+	/// <param name="x">영역의 가로 번호</param>
+	/// <param name="y">영역의 세로 번호</param>
+	/// <returns></returns>
+	constexpr float_pair PickPositionLast(int x, int y) const;
 };
