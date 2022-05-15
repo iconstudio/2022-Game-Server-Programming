69d9c0973c0da37be34e4264736bd4c35c7da9fe
Iconer <yoyofa2@hotmail.com>
2022-05-15 21:21:24 +0900

    서버에 필요한 헤더 삽입

--- Homeworks/Case05 - World/stdafx.h
+++ Homeworks/Case05 - World/stdafx.h
@@ -7,13 +7,49 @@
 
 #include <iostream>
 #include <memory>
+#include <numeric>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+#include <initializer_list>
+#include <array>
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-#include <numeric>
-#include <mutex>
-#include <chrono>
-#include <array>
+#include <concurrent_vector.h>
+
+using std::array;
+using std::shared_ptr;
+using std::unique_ptr;
+using std::weak_ptr;
+using std::atomic_bool;
+using std::atomic_int;
+using std::atomic_uint;
+
+using int_pair = std::pair<int, int>;
+using float_pair = std::pair<float, float>;
+
+template<typename Type>
+using shared_concurrent_vector = Concurrency::concurrent_vector<shared_ptr<Type>>;
+template<typename Type>
+using concurrent_vector = Concurrency::concurrent_vector<Type>;
+template<typename Type>
+using shared_concurrent_vector = concurrent_vector<shared_ptr<Type>>;
+template<typename Type>
+using weak_concurrent_vector = concurrent_vector<weak_ptr<Type>>;
+template<typename Type>
+using unique_concurrent_vector = concurrent_vector<unique_ptr<Type>>;
+
+template<typename Type>
+using shared_atomic = atomic<shared_ptr<Type>>;
+template<typename Type>
+using weak_atomic = atomic<weak_ptr<Type>>;
+
+template<typename Type>
+using shared_atomic_concurrent_vector = concurrent_vector<shared_atomic<Type>>;
+template<typename Type>
+using weak_atomic_concurrent_vector = concurrent_vector<weak_atomic<Type>>;
 
 class IOCPFramework;
 class Session;
