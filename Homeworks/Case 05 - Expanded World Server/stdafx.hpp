91866604ae179e5df31db7fa32185740df603cae
Iconer <yoyofa2@hotmail.com>
2022-05-15 20:00:03 +0900

    병렬성 자료구조와 원자적 자료구조 정의

--- Homeworks/Case 05 - Expanded World Server/stdafx.hpp
+++ Homeworks/Case 05 - Expanded World Server/stdafx.hpp
@@ -41,9 +41,23 @@ using int_pair = std::pair<int, int>;
 using float_pair = std::pair<float, float>;
 
 template<typename Type>
-using shared_concurrent_vector = Concurrency::concurrent_vector<shared_ptr<Type>>;
-template<typename Type>
 using concurrent_vector = Concurrency::concurrent_vector<Type>;
+template<typename Type>
+using shared_concurrent_vector = concurrent_vector<shared_ptr<Type>>;
+template<typename Type>
+using weak_concurrent_vector = concurrent_vector<weak_ptr<Type>>;
+template<typename Type>
+using unique_concurrent_vector = concurrent_vector<unique_ptr<Type>>;
+
+template<typename Type>
+using shared_atomic = atomic<shared_ptr<Type>>;
+template<typename Type>
+using weak_atomic = atomic<weak_ptr<Type>>;
+
+template<typename Type>
+using shared_atomic_concurrent_vector = concurrent_vector<shared_atomic<Type>>;
+template<typename Type>
+using weak_atomic_concurrent_vector = concurrent_vector<weak_atomic<Type>>;
 
 class IOCPFramework;
 class Timer;
