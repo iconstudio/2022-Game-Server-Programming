c648d61fe45743a9ef68689ae7626a7388faacb2
Iconer <yoyofa2@hotmail.com>
2022-05-19 04:43:22 +0900

    패킷 전송에 필요한 메서드 추가

--- Homeworks/Case05 - Server/Framework.hpp
+++ Homeworks/Case05 - Server/Framework.hpp
@@ -4,9 +4,9 @@
 #include "Session.h"
 
 const UINT THREADS_COUNT = 6;
-using SessionPtr = std::shared_ptr<Session>;
+using SessionPtr = shared_ptr<Session>;
 
-void IOCPWorker(const UINT index);
+void CALLBACK IOCPWorker();
 
 class IOCPFramework
 {
@@ -17,31 +17,65 @@ public:
 	void Init();
 	void Start();
 	void Update();
-	friend void IOCPWorker(const UINT index);
+	friend void CALLBACK IOCPWorker();
 
 	SessionPtr& GetClient(const UINT index);
 	SessionPtr& GetClientByID(const PID id);
-
-	UINT GetClientsNumber() const;
+	UINT GetClientsNumber() const volatile;
 
 	friend class Session;
 
 private:
 	void Listen();
 	bool ProceedAccept();
+	UINT GetAndAcquireClientsNumber() const volatile;
 
 	PID MakeNewbieID();
 	SessionPtr SeekNewbieSession() const;
-	void RegisterNewbie(const UINT index);
-
+	void ConnectFrom(const UINT index);
+	void SendWorldDataTo(SessionPtr& who);
 	void Disconnect(const PID who);
 
 	void ProceedPacket(LPWSAOVERLAPPED overlap, ULONG_PTR key, DWORD bytes);
-	void SendWorldDataTo(SessionPtr& who);
+
 	void BroadcastSignUp(SessionPtr& who);
 	void BroadcastSignOut(SessionPtr& who);
-	void BroadcastCreateCharacter(SessionPtr& who, CHAR cx, CHAR cy);
-	void BroadcastMoveCharacterFrom(const UINT index, CHAR nx, CHAR ny);
+
+	/// <summary>
+	/// 클라이언트에게 새로운 접속을 알리고, 로컬 플레이어 세션을 생성하도록 명령한다.
+	/// </summary>
+	/// <param name="target">클라이언트의 세션</param>
+	/// <param name="who">새로 접속한 플레이어의 세션</param>
+	int SendSignUp(SessionPtr& target, const SessionPtr& who);
+	/// <summary>
+	/// 클라이언트의 접속을 종료시키고, 다른 클라이언트에 알린다.
+	/// </summary>
+	/// <param name="target">클라이언트의 세션</param>
+	/// <param name="who">나가는 플레이어의 세션</param>
+	int SendSignOut(SessionPtr& target, const SessionPtr& who);
+	/// <summary>
+	/// 시야 내에 개체가 들어왔음을 알린다.
+	/// </summary>
+	/// <param name="target">클라이언트의 세션</param>
+	/// <param name="cid">NPC, 특수 객체, 플레이어의 고유 식별자</param>
+	/// <param name="type"></param>
+	/// <param name="cx"></param>
+	/// <param name="cy"></param>
+	int SendAppearEntity(SessionPtr& target, PID cid, int type, float cx, float cy);
+	/// <summary>
+	/// 시야 내에서 개체가 사라졌음을 알린다.
+	/// </summary>
+	/// <param name="target">클라이언트의 세션</param>
+	/// <param name="cid">NPC, 특수 객체, 플레이어의 고유 식별자</param>
+	int SendDisppearEntity(SessionPtr& target, PID cid);
+	/// <summary>
+	/// 개체의 이동을 알린다. (플레이어 자신도 포함)
+	/// </summary>
+	/// <param name="target">클라이언트의 세션</param>
+	/// <param name="cid">NPC, 특수 객체, 플레이어의 고유 식별자</param>
+	/// <param name="nx"></param>
+	/// <param name="ny"></param>
+	int SendMoveEntity(SessionPtr& target, PID cid, float nx, float ny);
 
 	SOCKET&& CreateSocket() const;
 
@@ -60,6 +94,7 @@ private:
 	atomic<SOCKET> acceptNewbie;
 
 	std::array<SessionPtr, CLIENTS_MAX_NUMBER> clientsPool;
+	std::concurr
 	atomic<PID> orderClientIDs;
 	atomic<UINT> numberClients;
 	std::timed_mutex mutexClient;
