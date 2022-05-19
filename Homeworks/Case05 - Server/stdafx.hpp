058bd362215ceed041b0e482d734ad356b74e7cf
Iconer <yoyofa2@hotmail.com>
2022-05-19 20:41:58 +0900

    서버에 월드의 정보를 추가

--- Homeworks/Case05 - Server/stdafx.hpp
+++ Homeworks/Case05 - Server/stdafx.hpp
@@ -114,14 +114,17 @@ constexpr PID CLIENTS_ORDER_BEGIN = 10000;
 constexpr UINT ENTITIES_MAX_NUMBER = CLIENTS_ORDER_BEGIN + 10000;
 constexpr UINT BUFFSIZE = 512;
 
-constexpr SIZE_T WND_SZ_W = 800, WND_SZ_H = 600; // Ã¢ Å©±â
-constexpr SIZE_T CELL_SIZE = 64;
+constexpr int CELL_SIZE = 32;
+
 constexpr int CELL_W = CELL_SIZE;
 constexpr int CELL_H = CELL_SIZE;
-constexpr int CELLS_CNT_H = 8;
-constexpr int CELLS_CNT_V = 8;
+constexpr int CELLS_CNT_H = 400;
+constexpr int CELLS_CNT_V = 400;
 constexpr int CELLS_LENGTH = CELLS_CNT_H * CELLS_CNT_V;
-constexpr int BOARD_W = CELL_W * CELLS_CNT_H;
-constexpr int BOARD_H = CELL_H * CELLS_CNT_V;
-constexpr int BOARD_X = (WND_SZ_W - BOARD_W) / 2;
-constexpr int BOARD_Y = (WND_SZ_H - BOARD_H - 20) / 2;
+constexpr int WORLD_W = CELL_W * CELLS_CNT_H;
+constexpr int WORLD_H = CELL_H * CELLS_CNT_V;
+
+constexpr int SIGHT_CELLS_CNT_H = 11;
+constexpr int SIGHT_CELLS_CNT_V = 11;
+constexpr int SIGHT_W = CELL_W * SIGHT_CELLS_CNT_H;
+constexpr int SIGHT_H = CELL_H * SIGHT_CELLS_CNT_V;
