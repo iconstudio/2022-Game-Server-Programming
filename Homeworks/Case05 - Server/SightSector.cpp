ca6325e8d087c59f64e3acad31c3fc10f963c3a1
Iconer <yoyofa2@hotmail.com>
2022-05-21 17:28:08 +0900

    시야 구역에서 객체를 넣고 빼는 메서드 추가

--- Homeworks/Case05 - Server/SightSector.cpp
+++ Homeworks/Case05 - Server/SightSector.cpp
@@ -9,6 +9,22 @@ SightSector::SightSector(int x, int y, float w, float h)
 SightSector::~SightSector()
 {}
 
+void SightSector::Add(const shared_ptr<GameEntity>& entity)
+{
+	Add(std::move(shared_ptr<GameEntity>(entity)));
+}
+
+void SightSector::Add(shared_ptr<GameEntity>&& entity)
+{
+	seeingInstances.emplace_back(std::forward<shared_ptr<GameEntity>>(entity));
+}
+
+void SightSector::Remove(const shared_ptr<GameEntity>& entity)
+{}
+
+void SightSector::Remove(shared_ptr<GameEntity>&& entity)
+{}
+
 bool SightSector::operator==(const SightSector& other) const noexcept
 {
 	return other.index_x == index_x && other.index_y == index_y;
--- Homeworks/Case05 - Server/SightSector.hpp
+++ Homeworks/Case05 - Server/SightSector.hpp
@@ -1,16 +1,23 @@
 #pragma once
 
+using shared_sight = shared_atomic<SightSector>;
+
 class SightSector
 {
 public:
 	SightSector(int x, int y, float w, float h);
 	~SightSector();
 
+	void Add(const shared_ptr<GameEntity>& entity);
+	void Add(shared_ptr<GameEntity>&& entity);
+	void Remove(const shared_ptr<GameEntity>& entity);
+	void Remove(shared_ptr<GameEntity>&& entity);
+
 	bool operator==(const SightSector& other) const noexcept;
 
 	const int index_x, index_y;
 
 private:
-	weak_concurrent_vector<GameObject> seeingInstances;
-	weak_concurrent_vector<GameObject>::iterator::difference_type seeingLast;
+	std::vector<weak_ptr<GameEntity>> seeingInstances;
+	std::vector<weak_ptr<GameEntity>>::iterator::difference_type seeingLast;
 };
