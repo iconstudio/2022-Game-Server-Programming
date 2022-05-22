39029f9afff69eea996987f7aa7ee8e171c1fb15
Iconer <yoyofa2@hotmail.com>
2022-05-22 20:41:33 +0900

    시야의 반지름 관련 전역 속성 추가

--- Homeworks/Case05 - Server/stdafx.hpp
+++ Homeworks/Case05 - Server/stdafx.hpp
@@ -37,5 +37,10 @@ constexpr float WORLD_H = CELL_H * WORLD_CELLS_CNT_V;
 
 constexpr int SIGHT_CELLS_CNT_H = 11;
 constexpr int SIGHT_CELLS_CNT_V = 11;
+constexpr int SIGHT_CELLS_RAD_H = SIGHT_CELLS_CNT_H / 2;
+constexpr int SIGHT_CELLS_RAD_V = SIGHT_CELLS_CNT_H / 2;
+
 constexpr float SIGHT_W = CELL_W * SIGHT_CELLS_CNT_H;
 constexpr float SIGHT_H = CELL_H * SIGHT_CELLS_CNT_V;
+constexpr float SIGHT_RAD_W = CELL_W * SIGHT_CELLS_RAD_H;
+constexpr float SIGHT_RAD_H = CELL_H * SIGHT_CELLS_RAD_V;
