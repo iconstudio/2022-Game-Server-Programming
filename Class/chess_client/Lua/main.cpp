05cac126a70c16deeeed72d5297e01873cc81f5a
Iconer <yoyofa2@hotmail.com>
2022-05-23 18:51:09 +0900

    lua 실습

--- Class/chess_client/Lua/main.cpp
+++ Class/chess_client/Lua/main.cpp
@@ -7,6 +7,16 @@ extern "C" {
 #include "lualib.h"
 }
 
+int test_addnum(lua_State* state)
+{
+	int lhs = int(lua_tonumber(state, -2));
+	int rhs = int(lua_tonumber(state, -1));
+	int result = lhs + rhs;
+	lua_pop(state, 3);
+	lua_pushnumber(state, result);
+	return 1;
+}
+
 int main()
 {
 	const char* command = "print \"Hello, world!\"";
@@ -14,19 +24,36 @@ int main()
 	lua_State* my_lua = luaL_newstate();
 	luaL_openlibs(my_lua);
 	luaL_loadfile(my_lua, "monster.lua");
+	lua_pcall(my_lua, 0, 0, 0);
 	//luaL_loadbuffer(my_lua, command, strlen(command), "line");
-
-	if (int error = lua_pcall(my_lua, 0, 0, 0))
+	
+	//
+	lua_getglobal(my_lua, "plustwo");
+	lua_pushnumber(my_lua, 100);
+	if (int error = lua_pcall(my_lua, 1, 1, 0))
 	{
 		std::cout << "Error! " << lua_tostring(my_lua, -1) << ".\n";
 		lua_pop(my_lua, 1);
 		return 1;
 	}
+	int plustwo_result = int(lua_tonumber(my_lua, -1));
+	std::cout << "plustwo: " << plustwo_result << "\n.";
+	lua_pop(my_lua, 1);
+
+	//
+	lua_register(my_lua, "test_addnum", test_addnum);
+	lua_getglobal(my_lua, "test_wrapper_addnum");
+	lua_pushnumber(my_lua, 300);
+	lua_pushnumber(my_lua, 400);
+	lua_pcall(my_lua, 2, 1, 0);
+	int ccall_result = int(lua_tonumber(my_lua, -1));
+	std::cout << "c call test: " << ccall_result << "\n.";
+	lua_pop(my_lua, 1);
 
 	int x_pos, y_pos;
 	lua_getglobal(my_lua, "pos_x");
 	lua_getglobal(my_lua, "pos_y");
-	
+
 	x_pos = lua_tonumber(my_lua, -2);
 	y_pos = lua_tonumber(my_lua, -1);
 	std::cout << "x: " << x_pos << ", y: " << y_pos << "\n";
--- Class/chess_client/Lua/monster.lua
+++ Class/chess_client/Lua/monster.lua
@@ -1,10 +1,14 @@
-function test_fun(value) 
+function plustwo(value) 
 	local a
 	a = 2
 	return value + a
 end
 
 pos_x = 6
-pos_y = test_fun(pos_x);
+pos_y = plustwo(pos_x);
+
+function test_wrapper_addnum(lhs, rhs)
+	return test_addnum(lhs, rhs)
+end
 
 -- C에서는 저장된 변수의 값을 읽어와야 한다.
