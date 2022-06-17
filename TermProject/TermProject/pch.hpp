948dec1d8a8c27d86ce14c300aa4a84d70447ebd
Iconer <yoyofa2@hotmail.com>
2022-06-17 23:00:45 +0900

    학기 프로젝트 수정

--- TermProject/TermProject/pch.hpp
+++ TermProject/TermProject/pch.hpp
@@ -5,13 +5,21 @@
 
 class Framework;
 class Packet;
+class Asynchron;
+enum PACKET_TYPES : unsigned char;
+enum OPERATION_TYPES : unsigned char;
+enum CHAT_TYPES : unsigned char;
+enum CLASS_TYPES : unsigned char;
 
 class ClientSlot;
 class Session;
+enum CLIENT_STATES : unsigned char;
 
 class GameObject;
-class GameEntity;
+class GameEntity; // place
 class PlayerCharacter;
+class NPC;
+class Monster;
 
 class Sprite;
 
--- TermProject/TermProject/stdafx.hpp
+++ TermProject/TermProject/stdafx.hpp
@@ -1,9 +1,5 @@
 #pragma once
-#pragma comment(lib, "MSWSock.lib")
-#pragma comment(lib, "Ws2_32.lib")
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <WS2tcpip.h>
-#include <MSWSock.h>
+#include <sdkddkver.h>
 
 #include <iostream>
 #include <memory>
@@ -19,20 +15,33 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-#include <cstdbool>
 #include <concurrent_vector.h>
+#include <boost/asio.hpp>
 
 using std::array;
 using std::shared_ptr;
 using std::unique_ptr;
 using std::weak_ptr;
 using std::atomic;
+using std::atomic_flag;
 using std::atomic_bool;
 using std::atomic_int;
 using std::atomic_uint;
 
 using int_pair = std::pair<int, int>;
 using float_pair = std::pair<float, float>;
+
+using boost::asio::ip::tcp;
+using boost::asio::ip::make_address;
+
+using NetService = boost::asio::io_context;
+using NetGate = boost::asio::ip::tcp::acceptor;
+using NetIPAddress = boost::asio::ip::address;
+using NetAddress = boost::asio::ip::tcp::endpoint;
+using NetErrorCode = boost::system::error_code;
+using Socket = boost::asio::ip::tcp::socket;
+
+using PID = size_t;
 
 template<typename Type>
 using shared_concurrent_vector = Concurrency::concurrent_vector<shared_ptr<Type>>;
@@ -54,6 +63,5 @@ template<typename Type>
 using shared_atomic_concurrent_vector = concurrent_vector<shared_atomic<Type>>;
 template<typename Type>
 using weak_atomic_concurrent_vector = concurrent_vector<weak_atomic<Type>>;
-
 
 
